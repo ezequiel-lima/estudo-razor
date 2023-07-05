7506ffc1fdbeb8106b60aae341ed885d5187b98e
ezequiel-lima <joao.lima94@fatec.sp.gov.br>
2023-07-05 10:29:08 -0300

    Adicionado categoria de teste

--- MyRazorApp/Pages/Index.cshtml.cs
+++ MyRazorApp/Pages/Index.cshtml.cs
@@ -6,15 +6,22 @@ namespace MyRazorApp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public List<Category> Categories { get; set; } = new();
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
         }
 
-        public void OnGet()
+        public async Task OnGet()
         {
-
+            for (int i = 0; i <= 100; i++)
+            {
+                Categories.Add(new Category(i, "Categoria " + i + 1, i * 2.31M));
+            }
         }
+
     }
 }
+
+public record Category(int Id, string Title, decimal Price);
